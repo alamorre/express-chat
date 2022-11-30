e25f9ed41803a6a6bc9acf71bf7504db9da500e0
Adam La Morre <alamorre@gmail.com>
2022-11-29 17:23:20 -0800

    No more strictmode for WS

--- frontend/src/app/chatsPage/index.js
+++ frontend/src/app/chatsPage/index.js
@@ -2,16 +2,14 @@ import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
   return (
-    <>
-      {JSON.stringify(props.user.secret)}
-      {JSON.stringify(props.user.username)}
-      {/* <PrettyChatWindow
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
         projectId="5d498a31-cd23-42b7-b367-4fcc9463bd2f"
         username={props.user.username}
         secret={props.user.secret}
         style={{ height: "100%" }}
-      /> */}
-    </>
+      />
+    </div>
   );
 };
 
